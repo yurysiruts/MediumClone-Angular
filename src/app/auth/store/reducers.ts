ccfd808945b5ecbb857faee65983ec02d8fc3a48
yurysiruts <sirut.2003@gmail.com>
2022-01-08 18:31:54 +0100

    updated auth/reducers on success & failure

--- src/app/auth/store/reducers.ts
+++ src/app/auth/store/reducers.ts
@@ -1,10 +1,17 @@
 import { state } from '@angular/animations';
 import { createReducer, on } from '@ngrx/store';
 import { AuthStateInterface } from '../types/authState.interface';
-import { registerAction } from './actions/register.action';
+import {
+  registerAction,
+  registerFailureAction,
+  registerSuccessAction,
+} from './actions/register.action';
 
 const initialState: AuthStateInterface = {
   isSubmitting: false,
+  currentUser: null,
+  isLoggedIn: null,
+  validationErrors: null,
 };
 
 export const authReducer = createReducer(
@@ -14,6 +21,25 @@ export const authReducer = createReducer(
     (state): AuthStateInterface => ({
       ...state,
       isSubmitting: true,
+      validationErrors: null,
+    })
+  ),
+  on(
+    registerSuccessAction,
+    (state, action): AuthStateInterface => ({
+      ...state,
+      isSubmitting: false,
+      isLoggedIn: true,
+      currentUser: action.currentUser,
+      validationErrors: null,
+    })
+  ),
+  on(
+    registerFailureAction,
+    (state, action): AuthStateInterface => ({
+      ...state,
+      isSubmitting: false,
+      validationErrors: action.errors,
     })
   )
 );
